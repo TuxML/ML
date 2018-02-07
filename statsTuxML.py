08a6f9a2ce4aa5392a01da892ceae3cee4d44013
Aexelion <aexelion@gmail.com>
2018-02-07 18:23:53 +0100

    debut lecture et analyse CSV

--- statsTuxML.py
+++ statsTuxML.py
@@ -4,9 +4,15 @@ import csv
 import argparse
 
 def stats(csvFilename):
-    res = csv.reader(open(csvFilename))
-
-
+    with open(csvFilename) as csvFile :
+        reader = csv.DictReader(csvFile)
+        nbLigne = 0
+        for ligne in reader :
+            nbLigne += 1
+            
+            ligne['KERNEL_SIZE'] = str(int(ligne['KERNEL_SIZE']) / (2**10))
+#            print(ligne['KERNEL_SIZE'])
+        return (nbLigne)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=
@@ -15,5 +21,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     stats(args.csv_filename)
+    
 
     
